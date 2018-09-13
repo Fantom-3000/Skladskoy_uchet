8ad1ad8379a2d42a164b03c05861ae1431b32f79
Fantom-3000 <Fantom-3000@users.noreply.github.com>
2018-09-13 20:49:00 +0300

    v. 18.9.13.2100

--- function.py
+++ function.py
@@ -13,41 +13,6 @@ def operation_date(unix_time):
     operation_date = time.strftime("%d.%m.%Y", time.localtime(unix_time))
     return operation_date
 
-# Вывод данных по транзвкциям
-def data_print(data):
-    col = TransactionTabel()
-    line_down() # ┌──────────┐
-    print('│Дата запроса: ' +
-          str(operation_date(time.time()).ljust(col.tabel_width-14, ' '))+'│')
-    line_transaction_down() # ├──┬───┬───┤
-    print('│'+'Дата'.center(col.date, ' ')+'│'+
-          'Тип'.center(col.type, ' ')+'│'+
-          'Наименование материала'.center(col.product_name, ' ')+'│'+
-          'Ед.изм.'.center(col.unit_name, ' ')+'│'+
-          'За ед.'.center(col.unit_price, ' ')+'│'+
-          'Кол-во'.center(col.amount, ' ')+'│'+
-          'Сумма'.center(col.total, ' ')+'│')
-    line_transaction_center() # ├───┼───┼───┤
-
-    for data_list in data:
-        transaction_date = operation_date(data_list[0])
-        transaction_type = data_list[1]
-        product_name = data_list[2]
-        unit_name = data_list[3]
-        unit_price = data_list[4]
-        amount = data_list[5]
-        # description = data_list[6]
-        total = unit_price * amount
-
-        print('│'+str(transaction_date.ljust(col.date, ' '))+'│'+
-              transaction_type.ljust(col.type, ' ')+'│'+
-              product_name.ljust(col.product_name, ' ')+'│'+
-              unit_name.center(col.unit_name, ' ')+'│'+
-              str(unit_price).rjust(col.unit_price, ' ')+'│'+
-              str(amount).rjust(col.amount, ' ')+'│'+
-              str(total).rjust(col.total, ' ')+'│')
-    line_transaction_up() # └──┴──┴──┘
-
 # Добавление новой транзакции
 def new_transaction():
     transaction_date = time.time()
--- transactions.py
+++ transactions.py
@@ -10,6 +10,7 @@ import time
 import function
 import re
 import os
+from settings import TransactionTabel
 
 # Подменю "Просмотр транзакций"
 def menu_view_transactions():
@@ -29,7 +30,42 @@ sql_start = """SELECT transactions.transaction_date, transactions.transaction_ty
                WHERE (transactions.id_product=products.id)
                    AND (transactions.transaction_type="""
 sql_end = """)ORDER BY transactions.transaction_date"""
-                                                
+
+# Вывод данных по транзвкциям
+def data_print(data):
+    col = TransactionTabel()
+    function.line_down() # ┌──────────┐
+    print('│Дата запроса: ' +
+          str(function.operation_date(time.time()).ljust(col.tabel_width-14, ' '))+'│')
+    function.line_transaction_down() # ├──┬───┬───┤
+    print('│'+'Дата'.center(col.date, ' ')+'│'+
+          'Тип'.center(col.type, ' ')+'│'+
+          'Наименование материала'.center(col.product_name, ' ')+'│'+
+          'Ед.изм.'.center(col.unit_name, ' ')+'│'+
+          'За ед.'.center(col.unit_price, ' ')+'│'+
+          'Кол-во'.center(col.amount, ' ')+'│'+
+          'Сумма'.center(col.total, ' ')+'│')
+    function.line_transaction_center() # ├───┼───┼───┤
+
+    for data_list in data:
+        transaction_date = function.operation_date(data_list[0])
+        transaction_type = data_list[1]
+        product_name = data_list[2]
+        unit_name = data_list[3]
+        unit_price = data_list[4]
+        amount = data_list[5]
+        # description = data_list[6]
+        total = unit_price * amount
+
+        print('│'+str(transaction_date.ljust(col.date, ' '))+'│'+
+              transaction_type.ljust(col.type, ' ')+'│'+
+              product_name.ljust(col.product_name, ' ')+'│'+
+              unit_name.center(col.unit_name, ' ')+'│'+
+              str(unit_price).rjust(col.unit_price, ' ')+'│'+
+              str(amount).rjust(col.amount, ' ')+'│'+
+              str(total).rjust(col.total, ' ')+'│')
+    function.line_transaction_up() # └──┴──┴──┘
+
 def view_transactions(cursor, os_clear):
     menu_view_transactions()
     while True:
@@ -51,7 +87,7 @@ def view_transactions(cursor, os_clear):
             sql = sql_start + """'Приход'""" + sql_end            
             cursor.execute(sql)
             data = cursor.fetchall()
-            function.data_print(data)
+            data_print(data)
             menu_view_transactions()
      
         # Вывод транзакций по категории "Расход"
@@ -60,7 +96,7 @@ def view_transactions(cursor, os_clear):
             sql = sql_start + """'Расход'""" + sql_end   
             cursor.execute(sql)
             data = cursor.fetchall()
-            function.data_print(data)
+            data_print(data)
             menu_view_transactions()
 
         # Вывод транзакций по категории "Перемещения"
@@ -69,5 +105,5 @@ def view_transactions(cursor, os_clear):
             sql = sql_start + """'Перемещ'""" + sql_end
             cursor.execute(sql)
             data = cursor.fetchall()
-            function.data_print(data)
+            data_print(data)
             menu_view_transactions()
