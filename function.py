31d36fd22948b37d38be7ed9e518585ecadb3d0a
Fantom-3000 <useahtung@gmail.com>
2018-09-13 15:58:46 +0300

    v. 18.9.13.1600 - Оптимизация кода

--- function.py
+++ function.py
@@ -15,156 +15,156 @@ def operation_date(unix_time):
 
 # Вывод данных по транзвкциям
 def data_print(data):
-      col = TransactionTabel()
-      line_down() # ┌──────────┐
-      print('│Дата запроса: ' +
-            str(operation_date(time.time()).ljust(col.tabel_width-14, ' '))+'│')
-      line_transaction_down() # ├──┬───┬───┤
-      print('│'+'Дата'.center(col.date, ' ')+'│'+
-            'Тип'.center(col.type, ' ')+'│'+
-            'Наименование материала'.center(col.product_name, ' ')+'│'+
-            'Ед.изм.'.center(col.unit_name, ' ')+'│'+
-            'За ед.'.center(col.unit_price, ' ')+'│'+
-            'Кол-во'.center(col.amount, ' ')+'│'+
-            'Сумма'.center(col.total, ' ')+'│')
-      line_transaction_center() # ├───┼───┼───┤
+    col = TransactionTabel()
+    line_down() # ┌──────────┐
+    print('│Дата запроса: ' +
+          str(operation_date(time.time()).ljust(col.tabel_width-14, ' '))+'│')
+    line_transaction_down() # ├──┬───┬───┤
+    print('│'+'Дата'.center(col.date, ' ')+'│'+
+          'Тип'.center(col.type, ' ')+'│'+
+          'Наименование материала'.center(col.product_name, ' ')+'│'+
+          'Ед.изм.'.center(col.unit_name, ' ')+'│'+
+          'За ед.'.center(col.unit_price, ' ')+'│'+
+          'Кол-во'.center(col.amount, ' ')+'│'+
+          'Сумма'.center(col.total, ' ')+'│')
+    line_transaction_center() # ├───┼───┼───┤
 
-      for data_list in data:
-            transaction_date = operation_date(data_list[0])
-            transaction_type = data_list[1]
-            product_name = data_list[2]
-            unit_name = data_list[3]
-            unit_price = data_list[4]
-            amount = data_list[5]
-            # description = data_list[6]
-            total = unit_price * amount
+    for data_list in data:
+        transaction_date = operation_date(data_list[0])
+        transaction_type = data_list[1]
+        product_name = data_list[2]
+        unit_name = data_list[3]
+        unit_price = data_list[4]
+        amount = data_list[5]
+        # description = data_list[6]
+        total = unit_price * amount
 
-            print('│'+str(transaction_date.ljust(col.date, ' '))+'│'+
-                  transaction_type.ljust(col.type, ' ')+'│'+
-                  product_name.ljust(col.product_name, ' ')+'│'+
-                  unit_name.center(col.unit_name, ' ')+'│'+
-                  str(unit_price).rjust(col.unit_price, ' ')+'│'+
-                  str(amount).rjust(col.amount, ' ')+'│'+
-                  str(total).rjust(col.total, ' ')+'│')
-      line_transaction_up() # └──┴──┴──┘
+        print('│'+str(transaction_date.ljust(col.date, ' '))+'│'+
+              transaction_type.ljust(col.type, ' ')+'│'+
+              product_name.ljust(col.product_name, ' ')+'│'+
+              unit_name.center(col.unit_name, ' ')+'│'+
+              str(unit_price).rjust(col.unit_price, ' ')+'│'+
+              str(amount).rjust(col.amount, ' ')+'│'+
+              str(total).rjust(col.total, ' ')+'│')
+    line_transaction_up() # └──┴──┴──┘
 
 # Добавление новой транзакции
 def new_transaction():
-      transaction_date = time.time()
-      transaction_type = input('Тип транзакции --> ')
-      product_name = input('Наименование материала --> ')
-      unit_name = input('Единица измерения --> ')
-      unit_price = float(input('Цена за единицу --> '))
-      amount = float(input('Количество --> '))
-      description = input('Назначение -->')
+    transaction_date = time.time()
+    transaction_type = input('Тип транзакции --> ')
+    product_name = input('Наименование материала --> ')
+    unit_name = input('Единица измерения --> ')
+    unit_price = float(input('Цена за единицу --> '))
+    amount = float(input('Количество --> '))
+    description = input('Назначение -->')
 
-      print(transaction_date)
+    print(transaction_date)
 
 # ┌───────┐
 def line_down():
-      col = TransactionTabel()
-      print('┌'+''.ljust(col.tabel_width, '─')+'┐')
+    col = TransactionTabel()
+    print('┌'+''.ljust(col.tabel_width, '─')+'┐')
 
 # ├───────┤
 def line_center():
-      col = TransactionTabel()
-      print('├'+''.ljust(col.tabel_width, '─')+'┤')
+    col = TransactionTabel()
+    print('├'+''.ljust(col.tabel_width, '─')+'┤')
 
 # └───────┘
 def line_up():
-      col = TransactionTabel()
-      print('└'+''.ljust(col.tabel_width, '─')+'┘')
+    col = TransactionTabel()
+    print('└'+''.ljust(col.tabel_width, '─')+'┘')
 
 # ├──┬──┬──┤
 def line_transaction_down():
-      col = TransactionTabel()
-      print('├'+''.ljust(col.date, '─')+'┬'+
-            ''.ljust(col.type, '─')+'┬'+
-            ''.ljust(col.product_name, '─')+'┬'+
-            ''.ljust(col.unit_name, '─')+'┬'+
-            ''.ljust(col.unit_price, '─')+'┬'+
-            ''.ljust(col.amount, '─')+'┬'+
-            ''.ljust(col.total, '─')+'┤')
+    col = TransactionTabel()
+    print('├'+''.ljust(col.date, '─')+'┬'+
+          ''.ljust(col.type, '─')+'┬'+
+          ''.ljust(col.product_name, '─')+'┬'+
+          ''.ljust(col.unit_name, '─')+'┬'+
+          ''.ljust(col.unit_price, '─')+'┬'+
+          ''.ljust(col.amount, '─')+'┬'+
+          ''.ljust(col.total, '─')+'┤')
 
 # ├──┼──┼──┤
 def line_transaction_center():
-      col = TransactionTabel()
-      print('├'+''.ljust(col.date, '─')+'┼'+
-            ''.ljust(col.type, '─')+'┼'+
-            ''.ljust(col.product_name, '─')+'┼'+
-            ''.ljust(col.unit_name, '─')+'┼'+
-            ''.ljust(col.unit_price, '─')+'┼'+
-            ''.ljust(col.amount, '─')+'┼'+
-            ''.ljust(col.total, '─')+'┤')
+    col = TransactionTabel()
+    print('├'+''.ljust(col.date, '─')+'┼'+
+          ''.ljust(col.type, '─')+'┼'+
+          ''.ljust(col.product_name, '─')+'┼'+
+          ''.ljust(col.unit_name, '─')+'┼'+
+          ''.ljust(col.unit_price, '─')+'┼'+
+          ''.ljust(col.amount, '─')+'┼'+
+          ''.ljust(col.total, '─')+'┤')
 
 # └──┴──┴──┘
 def line_transaction_up():
-      col = TransactionTabel()
-      print('└'+''.ljust(col.date, '─')+'┴'+
-            ''.ljust(col.type, '─')+'┴'+
-            ''.ljust(col.product_name, '─')+'┴'+
-            ''.ljust(col.unit_name, '─')+'┴'+
-            ''.ljust(col.unit_price, '─')+'┴'+
-            ''.ljust(col.amount, '─')+'┴'+
-            ''.ljust(col.total, '─')+'┘')
+    col = TransactionTabel()
+    print('└'+''.ljust(col.date, '─')+'┴'+
+          ''.ljust(col.type, '─')+'┴'+
+          ''.ljust(col.product_name, '─')+'┴'+
+          ''.ljust(col.unit_name, '─')+'┴'+
+          ''.ljust(col.unit_price, '─')+'┴'+
+          ''.ljust(col.amount, '─')+'┴'+
+          ''.ljust(col.total, '─')+'┘')
 
 # ┌──┬──┬──┐
 def line_product_down():
-      col = ProductTabel()
-      print('┌' + ''.ljust(col.card_number, '─') + '┬' +
-            ''.ljust(col.product_name, '─') + '┬' +
-            ''.ljust(col.unit_name, '─')+ '┬' +
-            ''.ljust(col.unit_price, '─') + '┐')
+    col = ProductTabel()
+    print('┌' + ''.ljust(col.card_number, '─') + '┬' +
+          ''.ljust(col.product_name, '─') + '┬' +
+          ''.ljust(col.unit_name, '─')+ '┬' +
+          ''.ljust(col.unit_price, '─') + '┐')
 
 # └──┴──┴──┘
 def line_product_up():
-      col = ProductTabel()
-      print('└'+''.ljust(col.card_number, '─')+'┴'+
-            ''.ljust(col.product_name, '─')+'┴'+
-            ''.ljust(col.unit_name, '─')+'┴'+
-            ''.ljust(col.unit_price, '─')+'┴'+
-            ''.ljust(col.balance, '─')+'┘')
+    col = ProductTabel()
+    print('└'+''.ljust(col.card_number, '─')+'┴'+
+          ''.ljust(col.product_name, '─')+'┴'+
+          ''.ljust(col.unit_name, '─')+'┴'+
+          ''.ljust(col.unit_price, '─')+'┴'+
+          ''.ljust(col.balance, '─')+'┘')
 
 # ├──┬──┬──┤
 def line_product_down_2():
-      col = ProductTabel()
-      print('├'+''.ljust(col.card_number, '─')+'┬'+
-            ''.ljust(col.product_name, '─')+'┬'+
-            ''.ljust(col.unit_name, '─')+'┬'+
-            ''.ljust(col.unit_price, '─')+'┬'+
-            ''.ljust(col.balance, '─')+'┤')
+    col = ProductTabel()
+    print('├'+''.ljust(col.card_number, '─')+'┬'+
+          ''.ljust(col.product_name, '─')+'┬'+
+          ''.ljust(col.unit_name, '─')+'┬'+
+          ''.ljust(col.unit_price, '─')+'┬'+
+          ''.ljust(col.balance, '─')+'┤')
 
 # ├──┼──┼──┤
 def line_product_center():
-      col = ProductTabel()
-      print('├'+''.ljust(col.card_number, '─') + '┼' +
-            ''.ljust(col.product_name, '─') + '┼' +
-            ''.ljust(col.unit_name, '─') + '┼' +
-            ''.ljust(col.unit_price, '─') + '┼' +
-            ''.ljust(col.balance, '─') + '┤')
+    col = ProductTabel()
+    print('├'+''.ljust(col.card_number, '─') + '┼' +
+          ''.ljust(col.product_name, '─') + '┼' +
+          ''.ljust(col.unit_name, '─') + '┼' +
+          ''.ljust(col.unit_price, '─') + '┼' +
+          ''.ljust(col.balance, '─') + '┤')
 
 # Вывод списка материалов
 def products_list(data):
-      col = ProductTabel()
-      line_down() # ┌───────┐
-      print('│'+'Дата запроса: ' + 
-            str(operation_date(time.time()).ljust(col.tabel_width-14, ' ')) + '│')
-      line_product_down_2() # ├──┬──┬──┤
-      print('│'+'Инв. ном.'.center(col.card_number, ' ') + '│' +
-            'Наименование материала'.center(col.product_name, ' ') + '│' +
-            'Ед. изм.'.center(col.unit_name, ' ') + '│' +
-            'За ед.'.center(col.unit_price, ' ')+'│'+
-            'Остаток'.center(col.balance, ' ')+'│')
-      line_product_center() # ├──┼──┼──┤
-      balance = '0.0'
-      for data_list in data:
-            card_number = data_list[0]
-            product_name = data_list[1]
-            unit_name = str(data_list[2])
-            unit_price = str(data_list[3])
-            print('│'+card_number.rjust(col.card_number, ' ') + '│' +
-                  product_name.ljust(col.product_name, ' ') + '│' +
-                  unit_name.center(col.unit_name, ' ') + '│' +
-                  unit_price.rjust(col.unit_price, ' ') + '│' +
-                  balance.rjust(col.balance, ' ') + '│')
-      line_product_up() # └──┴──┴──┘
+    col = ProductTabel()
+    line_down() # ┌───────┐
+    print('│'+'Дата запроса: ' + 
+          str(operation_date(time.time()).ljust(col.tabel_width-14, ' ')) + '│')
+    line_product_down_2() # ├──┬──┬──┤
+    print('│'+'Инв. ном.'.center(col.card_number, ' ') + '│' +
+          'Наименование материала'.center(col.product_name, ' ') + '│' +
+          'Ед. изм.'.center(col.unit_name, ' ') + '│' +
+          'За ед.'.center(col.unit_price, ' ')+'│'+
+          'Остаток'.center(col.balance, ' ')+'│')
+    line_product_center() # ├──┼──┼──┤
+    balance = '0.0'
+    for data_list in data:
+        card_number = data_list[0]
+        product_name = data_list[1]
+        unit_name = str(data_list[2])
+        unit_price = str(data_list[3])
+        print('│'+card_number.rjust(col.card_number, ' ') + '│' +
+              product_name.ljust(col.product_name, ' ') + '│' +
+              unit_name.center(col.unit_name, ' ') + '│' +
+              unit_price.rjust(col.unit_price, ' ') + '│' +
+              balance.rjust(col.balance, ' ') + '│')
+    line_product_up() # └──┴──┴──┘
--- settings.py
+++ settings.py
@@ -6,27 +6,27 @@ http://4pda.ru/forum/index.php?showuser=6015251
 """
 
 class TransactionTabel():
-      def __init__(self):
-            # Параметры таблицы
-            self.tabel_width = 85
+    def __init__(self):
+        # Параметры таблицы
+        self.tabel_width = 85
 
-            # Параметры колонок таблицы "Транзакции"
-            self.date = 10
-            self.type = 7
-            self.product_name = 27
-            self.unit_name = 10
-            self.unit_price = 8
-            self.amount = 7
-            self.total = 10
+        # Параметры колонок таблицы "Транзакции"
+        self.date = 10
+        self.type = 7
+        self.product_name = 27
+        self.unit_name = 10
+        self.unit_price = 8
+        self.amount = 7
+        self.total = 10
 
 class ProductTabel():
-      def __init__(self):
-            # Параметры таблицы
-            self.tabel_width = 85
+    def __init__(self):
+        # Параметры таблицы
+        self.tabel_width = 85
 
-            # Ширина колонок таблицы "Справочник материалов"
-            self.card_number = 9
-            self.product_name = 43
-            self.unit_name = 10
-            self.unit_price = 8
-            self.balance = 11
+        # Ширина колонок таблицы "Справочник материалов"
+        self.card_number = 9
+        self.product_name = 43
+        self.unit_name = 10
+        self.unit_price = 8
+        self.balance = 11
